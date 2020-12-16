fb3060c043d013d487aadf89f761a2819da836bd
exosite-RobertChen <robertchen@exosite.com>
2020-12-17 00:42:54 +0800

    Copy history done, research put copy history to the context menu

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Clippp
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClipboardListener = new WK.Libraries.SharpClipboardNS.SharpClipboard(this.components);
+            this.copyHistory = new System.Windows.Forms.ListBox();
+            this.quickPasteMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.測試ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.測試111ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.這也是測試ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickPasteMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ClipboardListener
+            // 
+            this.ClipboardListener.MonitorClipboard = true;
+            this.ClipboardListener.ObservableFormats.All = false;
+            this.ClipboardListener.ObservableFormats.Files = false;
+            this.ClipboardListener.ObservableFormats.Images = false;
+            this.ClipboardListener.ObservableFormats.Others = false;
+            this.ClipboardListener.ObservableFormats.Texts = true;
+            this.ClipboardListener.ObserveLastEntry = true;
+            this.ClipboardListener.Tag = null;
+            this.ClipboardListener.ClipboardChanged += new System.EventHandler<WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs>(this.ClipboardListener_ClipboardChanged);
+            // 
+            // copyHistory
+            // 
+            this.copyHistory.FormattingEnabled = true;
+            this.copyHistory.ItemHeight = 12;
+            this.copyHistory.Items.AddRange(new object[] {
+            "Your copy history"});
+            this.copyHistory.Location = new System.Drawing.Point(12, 12);
+            this.copyHistory.Name = "copyHistory";
+            this.copyHistory.Size = new System.Drawing.Size(776, 424);
+            this.copyHistory.TabIndex = 0;
+            this.copyHistory.SelectedIndexChanged += new System.EventHandler(this.copyHistory_SelectedIndexChanged);
+            // 
+            // quickPasteMenu
+            // 
+            this.quickPasteMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.測試ToolStripMenuItem,
+            this.這也是測試ToolStripMenuItem});
+            this.quickPasteMenu.Name = "quickPasteMenu";
+            this.quickPasteMenu.Size = new System.Drawing.Size(135, 48);
+            // 
+            // 測試ToolStripMenuItem
+            // 
+            this.測試ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.測試111ToolStripMenuItem});
+            this.測試ToolStripMenuItem.Name = "測試ToolStripMenuItem";
+            this.測試ToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.測試ToolStripMenuItem.Text = "測試";
+            // 
+            // 測試111ToolStripMenuItem
+            // 
+            this.測試111ToolStripMenuItem.Name = "測試111ToolStripMenuItem";
+            this.測試111ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.測試111ToolStripMenuItem.Text = "測試111";
+            // 
+            // 這也是測試ToolStripMenuItem
+            // 
+            this.這也是測試ToolStripMenuItem.Name = "這也是測試ToolStripMenuItem";
+            this.這也是測試ToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.這也是測試ToolStripMenuItem.Text = "這也是測試";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.copyHistory);
+            this.Name = "mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.quickPasteMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WK.Libraries.SharpClipboardNS.SharpClipboard ClipboardListener;
+        private System.Windows.Forms.ListBox copyHistory;
+        private System.Windows.Forms.ContextMenuStrip quickPasteMenu;
+        private System.Windows.Forms.ToolStripMenuItem 測試ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 測試111ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 這也是測試ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Clippp
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClipboardListener_ClipboardChanged(object sender, WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            var lastCopy = copyHistory.Items[0];
+            Console.WriteLine(lastCopy);
+            var copyText = Clipboard.GetText();
+            if (!lastCopy.Equals(copyText))
+            {
+                copyHistory.Items.Insert(0, Clipboard.GetText());
+            }
+        }
+
+        private void copyHistory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clipboard.SetText(copyHistory.SelectedItem.ToString());
+        }
+        class HotKey
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+            [Flags()]
+            public enum KeyModifiers
+            {
+                None = 0,
+                Alt = 1,
+                Ctrl = 2,
+                Shift = 4,
+                WindowsKey = 8
+            }
+        }
+
+        private void FrmSale_Leave(object sender, EventArgs e)
+        {
+            HotKey.UnregisterHotKey(Handle, 100);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case 100:
+                            var mouseX = Cursor.Position.X;
+                            var mouseY = Cursor.Position.Y;
+                            quickPasteMenu.Show(mouseX, mouseY);
+                            //QuickMenuForm form = new QuickMenuForm();
+                            //form.Show();
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            HotKey.RegisterHotKey(Handle, 100, HotKey.KeyModifiers.Alt, Keys.V);
+        }
+    }
+}
